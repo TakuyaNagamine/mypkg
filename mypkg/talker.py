0cbb0d70e4c209ecaea757d57e2c2f8bb4e72a44
Nagamine Takuya <s21c1090ke@s.chibakoudai.jp>
2023-01-01 20:36:48 +0900

    update talker,listener

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -3,17 +3,18 @@
 # SPDX-License-Identifier: BSD-3-Clause
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int16
+from person_msgs.msg import Person
 
 rclpy.init()
 node = Node("talker")
-pub = node.create_publisher(Int16, "countup", 10)
+pub = node.create_publisher(Person, "person", 10)
 n = 0
 
 def cb():
     global n
-    msg = Int16()
-    msg.data = n
+    msg = Person()
+    msg.name = "長峰拓也"
+    msg.age = n
     pub.publish(msg)
     n += 1
 
