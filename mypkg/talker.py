b6788dd81d13e5829162648945d24a939aa93a91
Nagamine Takuya <s21c1090ke@s.chibakoudai.jp>
2023-01-02 21:06:55 +0900

    update talker,listener

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -3,17 +3,25 @@
 # SPDX-License-Identifier: BSD-3-Clause
 import rclpy
 from rclpy.node import Node
-from person_msgs.srv import Query
+from std_msgs.msg import Int16
 
-def cb(request, response):
-    if request.name == "長峰拓也":
-        response.age = 19
-    else:
-        response.age = 255
-    
-    return response
+class Talker():
+    def __init__(self, node):
+        self.pub = node.create_publisher(Int16, "countup", 10)
+        self.n = 0
+        node.create_timer(0.5, self.cb)
 
-rclpy.init()
-node = Node("talker")
-srv = node.create_service(Query, "query", cb)
-rclpy.spin(node)
+    def cb(self):
+        msg = Int16()
+        msg.data = self.n
+        self.pub.publish(msg)
+        self.n += 1
+
+def main():
+    rclpy.init()
+    node = Node("talker")
+    talker = Talker(node)
+    rclpy.spin(node)
+
+if __name__ == '__main__':
+    main()
